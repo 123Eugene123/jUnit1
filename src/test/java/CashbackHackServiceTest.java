43f2f1e2cd8d927b8ecb76e7b61324458762fd69
Eugene test <eupushkash@ngs.ru>
2023-11-02 18:37:39 +0300

    jUnit4 commit

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -2,20 +2,20 @@ import ru.netology.service.CashbackHackService;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldFindRemainTestOne(){
-        org.testng.Assert.assertEquals(service.remain(999), 1);
+        org.junit.Assert.assertEquals(service.remain(999), 1);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldFindRemainTestTwo(){
-        org.testng.Assert.assertEquals(service.remain(1000), 0);
+        org.junit.Assert.assertEquals(service.remain(1000), 0);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldFindRemainTestThree(){
-        org.testng.Assert.assertEquals(service.remain(1001), 999);
+        org.junit.Assert.assertEquals(service.remain(1001), 999);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldFindRemainTestFour(){
-        org.testng.Assert.assertEquals(service.remain(0), 1000);
+        org.junit.Assert.assertEquals(service.remain(0), 1000);
     }
 }
